66fef3b29e0aefde29f4053ad65f70f175d24e19
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-09-12 13:49:06 +0530

    view changes

--- firebase_app/app/src/main/java/com/example/firebase_app/HomeActivity.java
+++ firebase_app/app/src/main/java/com/example/firebase_app/HomeActivity.java
@@ -1,21 +1,31 @@
 package com.example.firebase_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
 
     EditText name, age;
-    Button save;
+    Button save,view;
     DatabaseReference reff;
 
     Member member;
@@ -27,6 +37,7 @@ public class HomeActivity extends AppCompatActivity {
         name= findViewById(R.id.name);
         age=  findViewById(R.id.age);
         save= findViewById(R.id.save);
+        view= findViewById(R.id.view);
         member= new Member();
         reff= FirebaseDatabase.getInstance().getReference().child("Member");
 
@@ -48,9 +59,17 @@ public class HomeActivity extends AppCompatActivity {
 
                 reff.push().setValue(member);
 
-                Toast.makeText(HomeActivity.this, "successful entry", Toast.LENGTH_SHORT);
+                Toast.makeText(HomeActivity.this, "successful entry", Toast.LENGTH_SHORT).show();
 
 
+            }
+        });
+
+        //view data
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), view_data.class));
             }
         });
 
