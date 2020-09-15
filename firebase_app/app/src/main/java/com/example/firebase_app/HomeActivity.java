ddad28d60af6a48aa7ca514b2c095784953d7e90
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-09-16 00:23:07 +0530

    Add files via upload

--- firebase_app/app/src/main/java/com/example/firebase_app/HomeActivity.java
+++ firebase_app/app/src/main/java/com/example/firebase_app/HomeActivity.java
@@ -1,25 +1,17 @@
 package com.example.firebase_app;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -57,7 +49,7 @@ public class HomeActivity extends AppCompatActivity {
 
                 //push data
 
-                reff.push().setValue(member);
+                reff.child(name_var).setValue(member);
 
                 Toast.makeText(HomeActivity.this, "successful entry", Toast.LENGTH_SHORT).show();
 
